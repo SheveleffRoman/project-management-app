2e0ffad057d0fe589b4fd4062cf925afe793a824
Roman <@>
2023-07-31 14:58:38 +0400

    feat: add team, product, tech components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { ProductComponent } from './core/product/product.component';
+import { TechnologiesComponent } from './core/technologies/technologies.component';
+import { TeamComponent } from './core/team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ProductComponent,
+    TechnologiesComponent,
+    TeamComponent,
   ],
   imports: [
     BrowserModule,
